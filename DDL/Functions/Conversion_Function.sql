16ea26328f21c95eec761ac2f853f7445214eaff
Chris Ehmett <stanlumber@gmail.com>
2016-06-05 17:39:30 -0400

    Update Conversion_Function.sql

--- DDL/Functions/Conversion_Function.sql
+++ DDL/Functions/Conversion_Function.sql
@@ -2,13 +2,20 @@
 	Function to convert miles to meters
 	Function Name: fn_convert_radius
 	Author: Chris Ehmett
-	Last Updated: 06/03/2016
+	Last Updated: 06/05/2016
 ***************************************/
 
 CREATE OR REPLACE FUNCTION fn_convert_radius (p_miles INT)
 	RETURNS FLOAT
 AS $$
+BEGIN
 --multiply miles by meter conversion
-Select $1 * 1609.344;
+RETURN $1 * 1609.344;
+END
 $$ 
 LANGUAGE 'plpgsql';
+
+
+
+--Test slq
+---SELECT fn_convert_radius(25);
