7d3b3b916a7492fbdc203a0838b93fe86ce12434
Chris Ehmett <stanlumber@gmail.com>
2016-06-13 20:08:24 -0400

    Update Conversion_Function.sql

--- DDL/Functions/Conversion_Function.sql
+++ DDL/Functions/Conversion_Function.sql
@@ -5,7 +5,7 @@
 	Last Updated: 06/05/2016
 ***************************************/
 
-CREATE OR REPLACE FUNCTION fn_convert_radius (p_miles INT)
+CREATE OR REPLACE FUNCTION "OAR_ASP_DB".fn_convert_radius (p_miles INT)
 	RETURNS FLOAT
 AS $$
 BEGIN
