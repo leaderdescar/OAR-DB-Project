8eb01fc02521657e0fa5344aad7644412e6a7892
Chris Ehmett <stanlumber@gmail.com>
2016-06-22 11:58:46 -0400

    Update fn_mtg_schdl_wth_typ_optn.sql

--- DDL/Functions/fn_mtg_schdl_wth_typ_optn.sql
+++ DDL/Functions/fn_mtg_schdl_wth_typ_optn.sql
@@ -1,22 +1,22 @@
 /**************************************
 	Function Name: fn_mtg_schdl_wth_typ_optn 
 	Author: Chris Ehmett
-	Last Updated: 06/21/2016
+	Last Updated: 06/22/2016
 	The fn_mtg_schdl_wth_typ_optn function lists 
 	all providers or for only specific
 	types past
 ***************************************/
---DROP FUNCTION "OAR_OSP_DB".fn_mtg_schdl_wth_typ_optn(text[])
---SELECT * FROM "OAR_OSP_DB".fn_mtg_schdl_wth_typ_optn  ('{MD, SA, SFST}');
+--DROP FUNCTION "OAR_OSP_DB".fn_mtg_schdl_wth_typ_optn(TEXT[])
+--SELECT * FROM "OAR_OSP_DB".fn_mtg_schdl_wth_typ_optn  ('{DRA, SOS}');
 --SELECT * FROM "OAR_OSP_DB".fn_mtg_schdl_wth_typ_optn  ();
-CREATE OR REPLACE FUNCTION "OAR_OSP_DB".fn_mtg_schdl_wth_typ_optn (_p_mtg_typ text[] default '{}')
-	RETURNS TABLE (Provider_ID INTEGER, Latitude NUMBERIC, Longitude NUMERIC, Provider_Org_Name TEXT, Provider_Org_Alias TEXT, 
-					Phone_Number TEXT, Address_Line_1 TEXT, Address_Line_2 TEXT, Address_Line_3 TEXT, City_Name TEXT, State TEXT, Postal_code TEXT, 
-					Meeting_Type_Code TEXT, Meeting_Day TEXT, Meeting_Time TEXT, Meeting_Frequency TEXT)
+CREATE OR REPLACE FUNCTION "OAR_OSP_DB".fn_mtg_schdl_wth_typ_optn (_p_mtg_typ TEXT[] default '{}')
+	RETURNS TABLE (Provider_ID INTEGER, Latitude NUMERIC, Longitude NUMERIC, Provider_Org_Name VARCHAR, Provider_Org_Alias VARCHAR, 
+					Phone_Number CHARACTER, Address_Line_1 VARCHAR, Address_Line_2 VARCHAR, Address_Line_3 VARCHAR, City_Name VARCHAR, State CHARACTER, Postal_code CHARACTER, 
+					Meeting_Type_Code CHARACTER, Meeting_Day VARCHAR, Meeting_Time TIME, Meeting_Frequency VARCHAR)
 AS $$
 
 BEGIN
-	IF _p_mtg_typ  <> '{}'::text[] THEN
+	IF _p_mtg_typ  <> '{}'::TEXT[] THEN
 			RETURN QUERY SELECT
 						mtg.Provider_ID, mtg.Latitude, mtg.Longitude, mtg.Provider_Org_Name, mtg.Provider_Org_Alias, 
 						mtg.Phone_Number, mtg.Address_Line_1, mtg.Address_Line_2, mtg.Address_Line_3, mtg.City_Name, mtg.State, mtg.Postal_code, 
@@ -34,5 +34,3 @@ BEGIN
 END
 $$ 
 LANGUAGE 'plpgsql';
-
-
